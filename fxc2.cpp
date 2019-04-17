30098b6a77f0685853856b2cfd8b9d62284c7781
Martin Storsjö <martin@martin.st>
2019-04-17 22:50:26 +0300

    Don't free an error string before actually printing it
    Also skip making an unused std::string copy of it.

--- fxc2.cpp
+++ fxc2.cpp
@@ -264,9 +264,8 @@ int main(int argc, char* argv[])
      LPSTR messageBuffer = nullptr;
      size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
                                  NULL, hr, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
-     std::string message(messageBuffer, size);
-     LocalFree(messageBuffer);
      printf("Windows Error Message: %s\n", messageBuffer);
+     LocalFree(messageBuffer);
    }
 
    if (output)
